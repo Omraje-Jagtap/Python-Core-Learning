af3b131ac66c5951ab315ed487960051794f17d0
om jagtap <omjagtap1222008@gmail.com>
2026-07-04 16:24:48 +0530

    Add data_structure module with storing_in_list.py

---
+++ data_structure/storing_in_list.py
@@ -0,0 +1,10 @@
+
+# WAP to ask user to enter names of their 3 movie name & stores them in list.
+
+a = str(input("enter the first movie:"))
+b  = str(input("enter the( second movie: "))
+c = str(input("enter the third movie:"))
+
+list = [a,b,c]
+
+print(list)
