8d7983a54cddd82afdb874622d43e5b79fe8a92e
om jagtap <omjagtap1222008@gmail.com>
2026-07-07 22:59:59 +0530

    Add set_len.py to data_strcuture module

---
+++ data_structure/set_len.py
@@ -0,0 +1,12 @@
+#practice question:
+
+'''you are given a list of subject for all student. assume one classroom is
+   needed for one subject how many classroom are needed by all students :-
+   
+   "python","java","C++","pyhton","java","python","java","C++","C"'''
+
+
+
+
+set = {"python","java","C++","pyhton","java","python","java","C++","C"}
+print("the classroom is needed:",len(set))
