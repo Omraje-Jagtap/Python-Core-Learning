1bfe2388ec0deca025de50fd58618aecd7830c19
om jagtap <omjagtap1222008@gmail.com>
2026-07-14 23:13:56 +0530

    Add the dict_list.py to data_structure module

---
+++ data_structure/dict_list.py
@@ -0,0 +1,14 @@
+#practice question:
+
+'''store the word of following word meaning in pyhton dictonary
+
+    table : "a piece of furniture","list of fact" p"
+    cat   : "a small animal" '''
+
+dict ={
+    "table" : ["a piece of furniture","list of fact"],      #Here we used the list cause of we want to store the two values for a single dictonary
+    
+    "cat" : "a small animal",
+}
+
+print(dict)
