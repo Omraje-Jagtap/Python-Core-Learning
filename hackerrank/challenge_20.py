7eb37c12d630eed6b930dc021e28180b4ce2fc26
om jagtap <omjagtap1222008@gmail.com>
2026-07-29 16:25:02 +0530

    add challenge_20.py to hackerrank module

---
+++ hackerrank/challenge_20.py
@@ -0,0 +1,57 @@
+'''Task:
+Given an integer,n, print the following values for each integer i from 1 to n:
+Decimal
+Octal
+Hexadecimal (capitalized)
+Binary
+
+Prints:
+The four values must be printed on a single line in the order specified above for each i from 1 to number. 
+Each value should be space-padded to match the width of the binary value of number and the values should be separated by a single space.
+
+Input Format:
+A single integer denoting n.
+
+Constraints:
+1>= n <= 99
+
+Sample Input:
+17
+
+Sample Output:
+
+    1     1     1     1
+    2     2     2    10
+    3     3     3    11
+    4     4     4   100
+    5     5     5   101
+    6     6     6   110
+    7     7     7   111
+    8    10     8  1000
+    9    11     9  1001
+   10    12     A  1010
+   11    13     B  1011
+   12    14     C  1100
+   13    15     D  1101
+   14    16     E  1110
+   15    17     F  1111
+   16    20    10 10000
+   17    21    11 10001'''
+
+
+
+def print_formatted(number):
+    if number <= 99 and number >= 1:
+        width = len(bin(number)) - 2  
+    
+        for i in range(1, number + 1):
+            dec = str(i).rjust(width)
+            octal = oct(i)[2:].rjust(width)   
+            hexa = hex(i)[2:].upper().rjust(width)  
+            binary = bin(i)[2:].rjust(width) 
+            print(dec, octal, hexa, binary)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
